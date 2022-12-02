b24324aaab18ee8ab310453040e761aa1f44541f
RobertoSimonini <robertosimonini1@gmail.com>
2022-12-02 15:02:26 +0100

    element taken from dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-/*  Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21  */
+/* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21  */
 
-console.log ("OK JS")
+/*
+1. Prendo l'elemento dal dom 
+2. Chiedo il nome dell'utente
+3. Chiedo il cognome dell'utente
+4. Chiedo il colore preferito dell'utente
+5. Inserisco il tutto seguito da 21 nell'HTML
+
+*/
+
+const Passwordelement = document.getElementById ('Password');
